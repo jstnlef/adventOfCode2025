46cebc3c7b2a9a63362dc1009be53f472563b060
Justin LeFebvre <jstnlefebvre@gmail.com>
2025-12-03 01:51:31 -0500

    so close but not there yet

--- Day3.fs
+++ Day3.fs
@@ -2,12 +2,12 @@ module Day3
 
 open System.IO
 
-let findMax2BatteryJoltage (batteries: string) =
-  let findMaxIndex (batteries: string) =
-    seq { 9..-1..1 }
-    |> Seq.find (fun i -> batteries.IndexOf(i.ToString()) > -1)
-    |> (fun max -> batteries.IndexOf(max.ToString()))
+let findMaxIndex (batteries: string) =
+  seq { 9..-1..1 }
+  |> Seq.find (fun i -> batteries.IndexOf(i.ToString()) > -1)
+  |> (fun max -> batteries.IndexOf(max.ToString()))
 
+let findMax2BatteryJoltage (batteries: string) =
   let leftMaxIndex = findMaxIndex (batteries.Substring(0, batteries.Length - 1))
 
   let rightMaxIndex =
@@ -15,8 +15,25 @@ let findMax2BatteryJoltage (batteries: string) =
 
   int (System.String [| batteries[leftMaxIndex]; batteries[rightMaxIndex] |])
 
+let findMaxBatteryJoltage numBatteries (batteries: string) =
+  [| let mutable leftMostI = 0
+     let mutable found = 0
+
+     while found < numBatteries do
+       let subBattery = batteries.Substring(leftMostI, batteries.Length - numBatteries)
+
+       let maxIndex = findMaxIndex subBattery
+       found <- found + 1
+       yield batteries[leftMostI + maxIndex]
+       leftMostI <- leftMostI + maxIndex + 1 |]
+  |> System.String
+  |> int64
+
 let findTotalMax2BatteryJoltages (banks: string[]) =
   banks |> Array.sumBy findMax2BatteryJoltage
+
+let findTotalMaxBatteryJoltages numBatteries (banks: string[]) =
+  banks |> Array.sumBy (findMaxBatteryJoltage numBatteries)
 
 let parse filename = filename |> File.ReadAllLines
 
@@ -31,8 +48,8 @@ module Tests =
     Assert.Equal(expected, result)
 
   [<Theory>]
-  [<InlineData("Inputs/Day3/test.txt", -1)>]
+  [<InlineData("Inputs/Day3/test.txt", 3121910778619L)>]
   [<InlineData("Inputs/Day3/input.txt", -1)>]
-  let ``Part 2`` (filename: string, expected: int) =
-    let result = 0
+  let ``Find the maximum 12 battery output joltage`` (filename: string, expected: int64) =
+    let result = filename |> parse |> findTotalMaxBatteryJoltages 12
     Assert.Equal(expected, result)
