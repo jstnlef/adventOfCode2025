e2371402becb1fce24031c63e3391840e3c805fa
Justin <jstnlefebvre@gmail.com>
2025-12-05 10:26:25 -0500

    day 5 part 2 complete

--- Day5.fs
+++ Day5.fs
@@ -10,10 +10,23 @@ module IngredientDatabase =
   let isFresh (database: IngredientDatabase) (id: int64) =
     database.freshRanges |> Array.exists (fun (a, b) -> id >= a && id <= b)
 
-let countFreshIngredients (database: IngredientDatabase) =
-  database.ingredientIds
-  |> Array.Parallel.filter (IngredientDatabase.isFresh database)
-  |> _.Length
+  let countFreshIngredients (database: IngredientDatabase) =
+    database.ingredientIds |> Array.Parallel.filter (isFresh database) |> _.Length
+
+  let countAllPossibleFreshIngredients (database: IngredientDatabase) =
+    let inputRanges = database.freshRanges |> Array.sort
+    let mutable low, high = inputRanges[0]
+    let mutable count = 0L
+
+    for nl, nh in inputRanges[1..] do
+      if nl > high then
+        count <- count + high - low + 1L
+        low <- nl
+        high <- nh
+      else
+        high <- max high nh
+
+    count + high - low + 1L
 
 let parse filename =
   let split = filename |> File.ReadAllText |> _.Split("\n\n")
@@ -35,12 +48,14 @@ module Tests =
   [<InlineData("Inputs/Day5/test.txt", 3)>]
   [<InlineData("Inputs/Day5/input.txt", 623)>]
   let ``The number of fresh ingredient IDs`` (filename: string, expected: int) =
-    let result = filename |> parse |> countFreshIngredients
+    let result = filename |> parse |> IngredientDatabase.countFreshIngredients
     Assert.Equal(expected, result)
 
   [<Theory>]
-  [<InlineData("Inputs/Day5/test.txt", -1)>]
-  [<InlineData("Inputs/Day5/input.txt", -1)>]
-  let ``Part 2`` (filename: string, expected: int) =
-    let result = 0
+  [<InlineData("Inputs/Day5/test.txt", 14)>]
+  [<InlineData("Inputs/Day5/input.txt", 353507173555373L)>]
+  let ``The total number of ingredient IDs considered fresh`` (filename: string, expected: int64) =
+    let result =
+      filename |> parse |> IngredientDatabase.countAllPossibleFreshIngredients
+
     Assert.Equal(expected, result)
