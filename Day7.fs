7e8e839372c4262c0c49eab853be4733311fe2d0
Justin LeFebvre <jstnlefebvre@gmail.com>
2025-12-07 19:41:03 -0500

    Day7 part 2 complete

--- Day7.fs
+++ Day7.fs
@@ -1,33 +1,45 @@
 module Day7
 
 open System.Collections.Generic
+open Common.Functools
 
 type TachyonManifold =
   { start: int * int
     diagram: char array array }
 
-let countTachyonSplits (input: TachyonManifold) =
-  let mutable splits = 0
-  let frontier = Queue<int * int>([ input.start ])
-  let seen = HashSet<int * int>()
+module TachyonManifold =
+  let countTachyonSplits (manifold: TachyonManifold) =
+    let mutable splits = 0
+    let frontier = Queue<int * int>([ manifold.start ])
+    let seen = HashSet<int * int>()
 
-  while frontier.Count > 0 do
-    let r, c = frontier.Dequeue()
+    while frontier.Count > 0 do
+      let r, c = frontier.Dequeue()
 
-    if seen.Contains(r, c) |> not then
-      seen.Add(r, c) |> ignore
-      let nr = r + 1
+      if seen.Contains(r, c) |> not then
+        seen.Add(r, c) |> ignore
+        let nr = r + 1
 
-      if (nr < input.diagram.Length && c >= 0 && c < input.diagram[nr].Length) then
-        if input.diagram[nr][c] = '^' then
-          splits <- splits + 1
-          frontier.Enqueue(nr, c - 1)
-          frontier.Enqueue(nr, c + 1)
-        else
-          frontier.Enqueue(nr, c)
+        if (nr < manifold.diagram.Length && c >= 0 && c < manifold.diagram[nr].Length) then
+          if manifold.diagram[nr][c] = '^' then
+            splits <- splits + 1
+            frontier.Enqueue(nr, c - 1)
+            frontier.Enqueue(nr, c + 1)
+          else
+            frontier.Enqueue(nr, c)
 
-  splits
+    splits
 
+  let countPossibleTimelines (manifold: TachyonManifold) =
+    let rec count (memoizedCount: int * int -> int64) (r, c) =
+      if r = manifold.diagram.Length then
+        1L
+      elif manifold.diagram[r][c] = '^' then
+        memoizedCount (r, c - 1) + memoizedCount (r, c + 1)
+      else
+        memoizedCount (r + 1, c)
+
+    memoizeRec count manifold.start
 
 let parse filename : TachyonManifold =
   let diagram = System.IO.File.ReadAllLines filename |> Array.map _.ToCharArray()
@@ -43,12 +55,12 @@ module Tests =
   [<InlineData("Inputs/Day7/test.txt", 21)>]
   [<InlineData("Inputs/Day7/input.txt", 1560)>]
   let ``The number of times the tachyon beam splits`` (filename: string, expected: int) =
-    let result = filename |> parse |> countTachyonSplits
+    let result = filename |> parse |> TachyonManifold.countTachyonSplits
     Assert.Equal(expected, result)
 
   [<Theory>]
-  [<InlineData("Inputs/Day7/test.txt", -1)>]
-  [<InlineData("Inputs/Day7/input.txt", -1)>]
-  let ``Part 2`` (filename: string, expected: int) =
-    let result = 0
+  [<InlineData("Inputs/Day7/test.txt", 40)>]
+  [<InlineData("Inputs/Day7/input.txt", 25592971184998L)>]
+  let ``The number of different timelines a single tachyon particle could take`` (filename: string, expected: int64) =
+    let result = filename |> parse |> TachyonManifold.countPossibleTimelines
     Assert.Equal(expected, result)
