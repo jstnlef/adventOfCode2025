3ec4dcf20f71b2fc05af1d2344526b675c10dd2f
Justin <jstnlefebvre@gmail.com>
2025-12-09 17:32:33 -0500

    working on day 8

--- Common/Vector3d.fs
+++ Common/Vector3d.fs
@@ -2,8 +2,10 @@ module Common.Vector3d
 
 type Vector3d = int * int * int
 
-let distance (p1x, p1y, p1z) (p2x, p2y, p2z) : float =
+let distanceSquared (p1x, p1y, p1z) (p2x, p2y, p2z) : float =
   let dx = float (p1x - p2x)
   let dy = float (p1y - p2y)
   let dz = float (p1z - p2z)
-  sqrt ((dx * dx) + (dy * dy) + (dz * dz))
+  (dx * dx) + (dy * dy) + (dz * dz)
+
+let distance p1 p2 : float = sqrt (distanceSquared p1 p2)
--- Day8.fs
+++ Day8.fs
@@ -1,23 +1,22 @@
 module Day8
 
-open Common.Vector3d
+open Common
 open System.Collections.Generic
 
-type JunctionBox = Vector3d
+type JunctionBox = Vector3d.Vector3d
 type Circuit = HashSet<JunctionBox>
 
-let findCircuits (junctionBoxes: JunctionBox array) : Circuit array =
-  let mutable circuits = List<Circuit>()
+let multiplyLargestCircuits numberOfCircuits (junctionBoxes: JunctionBox array) =
+  let shortestDistanceJunctionBoxes =
+    seq {
+      for i in 0 .. junctionBoxes.Length - 1 do
+        for j in i + 1 .. junctionBoxes.Length - 1 do
+          yield junctionBoxes[i], junctionBoxes[j]
+    }
+    |> Seq.sortBy (fun (a, b) -> Vector3d.distanceSquared a b)
+    |> Seq.take numberOfCircuits
 
-  circuits.ToArray()
-
-let multiply3LargestCircuits junctionBoxes =
-  junctionBoxes
-  |> findCircuits
-  |> Array.map _.Count
-  |> Array.sortDescending
-  |> Array.take 3
-  |> Array.reduce (*)
+  0
 
 let parse filename : JunctionBox array =
   filename
@@ -29,10 +28,12 @@ module Tests =
   open Xunit
 
   [<Theory>]
-  [<InlineData("Inputs/Day8/test.txt", 40)>]
-  [<InlineData("Inputs/Day8/input.txt", -1)>]
-  let ``Multiply together the sizes of the three largest circuits`` (filename: string, expected: int) =
-    let result = filename |> parse |> multiply3LargestCircuits
+  [<InlineData("Inputs/Day8/test.txt", 10, 40)>]
+  [<InlineData("Inputs/Day8/input.txt", 1000, -1)>]
+  let ``Multiply together the sizes of the three largest circuits``
+    (filename: string, numberOfCircuits: int, expected: int)
+    =
+    let result = filename |> parse |> multiplyLargestCircuits numberOfCircuits
     Assert.Equal(expected, result)
 
   [<Theory>]
