e7a09760d346fdfd82992811a22e5a9154d61ab6
Justin <jstnlefebvre@gmail.com>
2025-12-15 15:18:09 -0500

    playing with Skia

--- Day9.fs
+++ Day9.fs
@@ -1,19 +1,48 @@
 module Day9
 
 open System.IO
+open SkiaSharp
 
-let findLargestAreaRectangle (corners: (int64 * int64) array) : int64 =
-  let computeArea ((x1, y1), (x2, y2)) =
-    (abs (x2 - x1 + 1L)) * (abs (y2 - y1 + 1L))
+let width (x1, _) (x2, _) = (abs x2 - x1) + 1
+let height (_, y1) (_, y2) = (abs y2 - y1) + 1
 
+let computeArea (a, b) : int64 =
+  (int64 (width a b)) * (int64 (height a b))
+
+let findLargestAreaRectangle (corners: (int * int) array) : int64 =
   (corners, corners) ||> Array.allPairs |> Array.map computeArea |> Array.max
+
+let findLargestAreaRectangleWithRedGreenTiles (corners: (int * int) array) : int64 =
+  let path = new SKPath()
+  let sx, sy = corners[0]
+  path.MoveTo(float32 sx, float32 sy)
+
+  for x, y in corners[1..] do
+    path.LineTo(float32 x, float32 y)
+
+  let inArea ((x1, y1), (x2, y2)) =
+    let left = min x1 x2 |> float32
+    let right = max x1 x2 |> float32
+    let bottom = min y1 y2 |> float32
+    let top = max y1 y2 |> float32
+
+    [| for x in left..right do
+         for y in bottom..top do
+           yield x, y |]
+    |> Array.Parallel.forall (fun (x, y) -> path.Bounds.Contains(x, y))
+
+  (corners, corners)
+  ||> Array.allPairs
+  |> Array.filter inArea
+  |> Array.map computeArea
+  |> Array.max
 
 let parse filename =
   filename
   |> File.ReadAllLines
   |> Array.map (fun l ->
     let s = l.Split(",")
-    int64 s[0], int64 s[1])
+    int s[0], int s[1])
 
 module Tests =
   open Xunit
@@ -29,7 +58,7 @@ module Tests =
   [<InlineData("Inputs/Day9/test.txt", 24)>]
   [<InlineData("Inputs/Day9/input.txt", -1)>]
   let ``Part 2: Find then largest area of any rectangle using only red and green tiles``
-    (filename: string, expected: int)
+    (filename: string, expected: int64)
     =
-    let result = 0
+    let result = filename |> parse |> findLargestAreaRectangleWithRedGreenTiles
     Assert.Equal(expected, result)
